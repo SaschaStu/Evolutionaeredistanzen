f2d5484727af800be97ec2444d0386fdbfad3744
yannikt79 <yannik.tobler@kbw.ch>
2021-12-06 10:27:29 +0100

    first implementation

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,20 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+
 
 
     public static void main(String[] args) {
-        launch(args);
+        String x = "ATG";
+        String y = "TCA";
+
+        String[] strain = x.split("",0);
+        String[] newStrain = new String[5];
+
+        for(int i=0; i< x.length();i++){
+            System.out.println(strain[i]);
+        }
     }
 }
